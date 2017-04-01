d448ee09542a537069c6be3eae1d5bc28f0bd8aa
litanontaru <litanontaru@yahoo.com>
2017-04-01 07:58:16 +0400

    54 implemented first vision on callable task - not threadsafe

---
+++ lagerta/src/main/java/com/epam/lathgertha/common/CallableTask.java
@@ -0,0 +1,28 @@
+package com.epam.lathgertha.common;
+
+import java.util.function.BiFunction;
+
+/**
+ * @author Andrei_Yakushin
+ * @since 01.04.2017 7:43
+ */
+public class CallableTask<T, V> {
+    private final Scheduler scheduler;
+    private final BiFunction<V, T, V> appender;
+
+    private V value;
+
+    public CallableTask(Scheduler scheduler, BiFunction<V, T, V> appender) {
+        this.scheduler = scheduler;
+        this.appender = appender;
+    }
+
+    public void append(T value) {
+        this.value = appender.apply(this.value, value);
+    }
+
+    public V call(Runnable runnable) throws Exception {
+        scheduler.pushTask(runnable);
+        return value;
+    }
+}
