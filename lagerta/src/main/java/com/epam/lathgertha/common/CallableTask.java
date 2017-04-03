5feb63683b7840550f1d1bc139741b96d47f492d
Andrei_Yakushin <Andrei_Yakushin@epam.com>
2017-04-03 11:50:34 +0400

    54 added copyright and fixed javadoc

--- lagerta/src/main/java/com/epam/lathgertha/common/CallableTask.java
+++ lagerta/src/main/java/com/epam/lathgertha/common/CallableTask.java
@@ -1,12 +1,23 @@
+/*
+ *  Copyright 2017 EPAM Systems.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package com.epam.lathgertha.common;
 
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.BiFunction;
 
-/**
- * @author Andrei_Yakushin
- * @since 01.04.2017 7:43
- */
 public class CallableTask<V, T> {
     private final Scheduler scheduler;
     private final BiFunction<V, T, V> appender;
