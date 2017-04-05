6ce6d7fe6d5b2e53a79453065b0ff4575ccc5687
Anton Solovev <anton_solovev@epam.com>
2017-04-05 16:32:29 +0400

    75 fixed lead fat util test

--- lagerta/src/test/java/com/epam/lathgertha/subscriber/lead/LeadImplFatUnitTest.java
+++ lagerta/src/test/java/com/epam/lathgertha/subscriber/lead/LeadImplFatUnitTest.java
@@ -19,16 +19,15 @@ package com.epam.lathgertha.subscriber.lead;
 import com.epam.lathgertha.capturer.TransactionScope;
 import com.google.common.util.concurrent.Uninterruptibles;
 import org.testng.Assert;
+import org.testng.AssertJUnit;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.List;
 import java.util.UUID;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.TimeUnit;
-import java.util.function.BooleanSupplier;
 
 import static com.epam.lathgertha.subscriber.DataProviderUtil.cacheScope;
 import static com.epam.lathgertha.subscriber.DataProviderUtil.list;
@@ -37,7 +36,10 @@ import static org.mockito.Mockito.mock;
 
 public class LeadImplFatUnitTest {
 
-    private static final LeadStateAssistantImpl MOCK_STATE_ASSISTANT = mock(LeadStateAssistantImpl.class);
+    private static final long TIMEOUT = 1000L;
+    private static final int INVOCATION_COUNT = 500;
+
+    private static final LeadStateAssistant MOCK_STATE_ASSISTANT = mock(LeadStateAssistant.class);
 
     private static final UUID A = UUID.randomUUID();
     private static final UUID B = UUID.randomUUID();
@@ -46,19 +48,13 @@ public class LeadImplFatUnitTest {
     private static final String CACHE2 = "cache2";
 
     private LeadImpl lead;
-    private ReadTransactions read;
-    private CommittedTransactions commit;
-    private DynamicRule dynamicRule;
 
     @BeforeMethod
     public void setUp() throws Exception {
-        read = new ReadTransactions();
-        commit = new CommittedTransactions();
-        dynamicRule = new DynamicRule();
-        dynamicRule.setPredicate(() -> true);
-        lead = new LeadImpl(MOCK_STATE_ASSISTANT, read, commit);
-        lead.updateState(commit);
-        lead.registerRule(dynamicRule);
+        ReadTransactions read = new ReadTransactions();
+        CommittedTransactions committed = new CommittedTransactions();
+        lead = new LeadImpl(MOCK_STATE_ASSISTANT, read, committed);
+        lead.updateState(committed);
         ForkJoinPool.commonPool().submit(() -> lead.execute());
     }
 
@@ -67,7 +63,7 @@ public class LeadImplFatUnitTest {
         lead.stop();
     }
 
-    @Test
+    @Test(timeOut = TIMEOUT)
     public void regressionTestOnBlockedTransactionsLogicInPlanner() {
         List<TransactionScope> aScope = list(
                 txScope(0, cacheScope(CACHE2, 1L)),
@@ -76,20 +72,13 @@ public class LeadImplFatUnitTest {
         List<TransactionScope> bScope = list(
                 txScope(1, cacheScope(CACHE2, 2L)));
 
-        applyStatements(
-                () -> notifyRead(B, bScope, -2),
-                () -> notifyRead(A, aScope),
-                () -> Uninterruptibles.sleepUninterruptibly(1_000, TimeUnit.MILLISECONDS),
-                () -> assertEquals(notifyRead(A, list()), list(0L)));
-    }
-
-    private List<Long> notifyRead(UUID uuid, List<TransactionScope> scope, long previousLastDenseRead) {
-        dynamicRule.setPredicate(() -> read.getLastDenseRead() > previousLastDenseRead);
-        return lead.notifyRead(uuid, scope);
+        notifyRead(B, bScope);
+        notifyRead(A, aScope);
+        assertPlanned(A, list(0L));
     }
 
     // (0 -> 2) + (1 -> 2)
-    @Test
+    @Test(timeOut = TIMEOUT)
     public void sequenceBlockedFromOutside() {
         List<TransactionScope> aScope = list(
                 txScope(0, cacheScope(CACHE2, 1L)),
@@ -98,35 +87,33 @@ public class LeadImplFatUnitTest {
         List<TransactionScope> bScope = list(
                 txScope(1, cacheScope(CACHE2, 2L)));
 
-        applyStatements(
-                () -> notifyRead(A, aScope),
-                () -> notifyRead(B, bScope),
-                () -> assertEquals(notifyRead(A, list()), list(0L)),
-                () -> notifyCommitted(A, list(0L)),
-                () -> assertEquals(notifyRead(B, list()), list(1L)),
-                () -> notifyCommitted(B, list(1L)),
-                () -> assertEquals(notifyRead(A, list()), list(2L)));
+        notifyRead(A, aScope);
+        notifyRead(B, bScope);
+        assertPlanned(A, list(0L));
+        notifyCommitted(A, list(0L));
+        waitForLastDenseCommitted(0L);
+        assertPlanned(B, list(1L));
+        notifyCommitted(B, list(1L));
+        waitForLastDenseCommitted(1L);
+        assertPlanned(A, list(2L));
     }
 
     //    (0 -> 1 -> 2) + (1 -> 3) + (4 -> 5 -> 6) + (7 -> 8 -> 9)
     //      + (9 -> 10 -> 11) + (12 -> 14) + (13 -> 14)
-    @Test
+    @Test(invocationCount = INVOCATION_COUNT, timeOut = TIMEOUT)
     public void forksJoinsAndBlockedPaths() {
         List<TransactionScope> aScope = list(
                 txScope(0, cacheScope(CACHE1, 1L)),
                 txScope(1, cacheScope(CACHE1, 1L, 2L)),
                 txScope(2, cacheScope(CACHE1, 1L)),
                 txScope(3, cacheScope(CACHE1, 2L)),
-
                 txScope(4, cacheScope(CACHE1, 3L)),
                 txScope(5, cacheScope(CACHE1, 3L)),
                 txScope(6, cacheScope(CACHE1, 3L, 4L)),
                 txScope(7, cacheScope(CACHE1, 4L)),
                 txScope(8, cacheScope(CACHE1, 4L)),
-
                 txScope(9, cacheScope(CACHE1, 5L)),
                 txScope(11, cacheScope(CACHE1, 5L)),
-
                 txScope(12, cacheScope(CACHE2, 1L)),
                 txScope(14, cacheScope(CACHE2, 1L, 2L)));
 
@@ -134,74 +121,54 @@ public class LeadImplFatUnitTest {
                 txScope(10, cacheScope(CACHE1, 5L)),
                 txScope(13, cacheScope(CACHE2, 2L)));
 
-        List<Long> expectedFirstCommits = list(0L, 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 12L, 14L);
-        applyStatements(
-                () -> notifyRead(A, aScope),
-                () -> notifyRead(B, bScope),
-                () -> notifyCommitted(A, list()),
-                () -> assertEquals(notifyRead(B, list()), list(13L)),
-                () -> assertEquals(notifyRead(B, list()), list()),
-                () -> assertEquals(notifyRead(A, list()), expectedFirstCommits),
-                () -> assertEquals(notifyRead(A, list()), list()),
-                () -> notifyCommitted(B, list(13L)),
-                () -> assertEquals(notifyRead(B, list()), list()),
-                () -> notifyCommitted(A, expectedFirstCommits),
-                () -> assertEquals(notifyRead(A, list()), list()),
-                () -> assertEquals(notifyRead(B, list()), list(10L)),
-                () -> notifyCommitted(B, list(10L)),
-                () -> assertEquals(notifyRead(A, list()), list(11L)),
-                () -> notifyCommitted(A, list(11L)),
-                () -> Assert.assertTrue(commit.getLastDenseCommit() == 14L)
-        );
+        List<Long> expectedFirstCommits = list(0L, 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 12L);
+
+        notifyRead(A, aScope);
+        notifyRead(B, bScope);
+        assertPlanned(B, list(13L));
+        assertEquals(notifyRead(B, list()), list());
+        assertPlanned(A, expectedFirstCommits);
+        assertEquals(notifyRead(A, list()), list());
+        notifyCommitted(B, list(13L));
+        assertEquals(notifyRead(B, list()), list());
+        notifyCommitted(A, expectedFirstCommits);
+        assertPlanned(A, list(14L));
+        assertPlanned(B, list(10L));
+        notifyCommitted(B, list(10L));
+        assertPlanned(A, list(11L));
+        notifyCommitted(A, list(11L));
+        waitForLastDenseCommitted(13L);
+        Assert.assertTrue(lead.getLastDenseCommitted() == 13L);
+    }
+
+    private void assertPlanned(UUID uuid, List<Long> nonEmptyExpected) {
+        List<Long> actual;
+        do {
+            Uninterruptibles.sleepUninterruptibly(10, TimeUnit.MILLISECONDS);
+            actual = notifyRead(uuid, list());
+        } while (actual.isEmpty());
+        assertEquals(actual, nonEmptyExpected);
+    }
+
+    private List<Long> notifyRead(UUID uuid, List<TransactionScope> scope) {
+        return lead.notifyRead(uuid, scope);
+    }
+
+    private void notifyCommitted(UUID readerId, List<Long> commits) {
+        lead.notifyCommitted(readerId, commits);
+    }
+
+    private void waitForLastDenseCommitted(long expected) {
+        long actual;
+        do {
+            Uninterruptibles.sleepUninterruptibly(10, TimeUnit.MILLISECONDS);
+            actual = lead.getLastDenseCommitted();
+        } while (actual != expected);
     }
 
     private static void assertEquals(List<Long> actual, List<Long> expected) {
         actual.sort(Long::compareTo);
         expected.sort(Long::compareTo);
-        Assert.assertEquals(actual, expected);
-    }
-
-    private List<Long> notifyRead(UUID uuid, List<TransactionScope> scope) {
-        dynamicRule.setPredicate(() -> read.getLastDenseRead() > -1);
-        return lead.notifyRead(uuid, scope);
-    }
-
-    private void notifyCommitted(UUID readerId, List<Long> committed) {
-        lead.notifyCommitted(readerId, committed);
-        dynamicRule.setPredicate(() -> committed.isEmpty() ||
-                committed.stream().anyMatch(commit::contains));
-    }
-
-    private void applyStatements(Runnable... statements) {
-        for (Runnable run : statements) {
-            dynamicRule.await();
-            run.run();
-        }
-    }
-
-    private static class DynamicRule implements Runnable {
-
-        private CountDownLatch latch = new CountDownLatch(1);
-        private BooleanSupplier predicate;
-
-        @Override
-        public void run() {
-            if (predicate.getAsBoolean()) {
-                latch.countDown();
-            }
-        }
-
-        void await() {
-            try {
-                latch.await();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
-        void setPredicate(BooleanSupplier predicate) {
-            this.predicate = predicate;
-            this.latch = new CountDownLatch(1);
-        }
+        AssertJUnit.assertEquals(expected, actual);
     }
 }
