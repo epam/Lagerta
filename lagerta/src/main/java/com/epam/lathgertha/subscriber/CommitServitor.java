f774998940a1eef7bd42b45cbbec230194c20989
Andrei_Yakushin <Andrei_Yakushin@epam.com>
2017-04-12 12:48:17 +0400

    100 added log

--- lagerta/src/main/java/com/epam/lathgertha/subscriber/CommitServitor.java
+++ lagerta/src/main/java/com/epam/lathgertha/subscriber/CommitServitor.java
@@ -4,6 +4,8 @@ import com.epam.lathgertha.kafka.KafkaLogCommitter;
 import com.epam.lathgertha.services.LeadService;
 import com.epam.lathgertha.util.Serializer;
 import org.apache.ignite.Ignite;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Iterator;
 import java.util.List;
@@ -14,6 +16,8 @@ import java.util.UUID;
  * Utilize logic of single transaction commit and write to local kafka log
  */
 public class CommitServitor {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CommitServitor.class);
+
     private final Committer committer;
     private final Serializer serializer;
     private final KafkaLogCommitter kafkaLogCommitter;
@@ -43,6 +47,7 @@ public class CommitServitor {
             kafkaLogCommitter.commitTransaction(txId);
             return true;
         } catch (Exception e) {
+            LOGGER.error("[R] error while commit transaction in " + readerId, e);
             lead.notifyFailed(readerId, txId);
             return false;
         }
