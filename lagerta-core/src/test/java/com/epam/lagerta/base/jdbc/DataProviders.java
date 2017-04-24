fbee2748e235fbe32cdaa74a20c6c257243ea3ec
Evgeniy Ignatiev <evgeniy_ignatiev@epam.com>
2017-04-24 13:53:02 +0400

    162 Remove redundant `new Object[]` calls and redundant method `data`

--- lagerta-core/src/test/java/com/epam/lagerta/base/jdbc/DataProviders.java
+++ lagerta-core/src/test/java/com/epam/lagerta/base/jdbc/DataProviders.java
@@ -25,8 +25,9 @@ import org.apache.ignite.Ignite;
 
 import java.math.BigDecimal;
 import java.sql.Timestamp;
-import java.util.Arrays;
 import java.util.Date;
+
+import static java.util.Arrays.asList;
 
 public class DataProviders {
     public static final String KV_META_PROVIDER = "kvMetaProvider";
@@ -52,47 +53,43 @@ public class DataProviders {
     // ToDo: Uncomment all commented out cases after fixes to issue #180.
     public static Object[][] provideKVMeta(Ignite ignite) {
         return new Object[][] {
-                new Object[] {PrimitivesHolder.withMetaData(1, PH_1)},
-                new Object[] {PrimitivesHolder.withMetaData(1, PH_2)},
-                new Object[] {PrimitivesHolder.withMetaData(1, binary(ignite, PH_1))},
-                new Object[] {PrimitivesHolder.withMetaData(1, binary(ignite, PH_1))},
-                new Object[] {PrimitiveWrappersHolder.withMetaData(1, PWH_1)},
-                new Object[] {PrimitiveWrappersHolder.withMetaData(2, PWH_2)},
-                new Object[] {PrimitiveWrappersHolder.withMetaData(3, PWH_3)},
-                new Object[] {PrimitiveWrappersHolder.withMetaData(1, binary(ignite, PWH_1))},
-                new Object[] {PrimitiveWrappersHolder.withMetaData(2, binary(ignite, PWH_2))},
-                new Object[] {PrimitiveWrappersHolder.withMetaData(3, binary(ignite, PWH_3))},
-                new Object[] {OtherTypesHolder.withMetaData(1, OTH_1)},
-                new Object[] {OtherTypesHolder.withMetaData(1, OTH_2)},
-                //new Object[] {OtherTypesHolder.withMetaData(1, binary(ignite, OTH_1))},
-                new Object[] {OtherTypesHolder.withMetaData(1, binary(ignite, OTH_2))}
+                {PrimitivesHolder.withMetaData(1, PH_1)},
+                {PrimitivesHolder.withMetaData(1, PH_2)},
+                {PrimitivesHolder.withMetaData(1, binary(ignite, PH_1))},
+                {PrimitivesHolder.withMetaData(1, binary(ignite, PH_1))},
+                {PrimitiveWrappersHolder.withMetaData(1, PWH_1)},
+                {PrimitiveWrappersHolder.withMetaData(2, PWH_2)},
+                {PrimitiveWrappersHolder.withMetaData(3, PWH_3)},
+                {PrimitiveWrappersHolder.withMetaData(1, binary(ignite, PWH_1))},
+                {PrimitiveWrappersHolder.withMetaData(2, binary(ignite, PWH_2))},
+                {PrimitiveWrappersHolder.withMetaData(3, binary(ignite, PWH_3))},
+                {OtherTypesHolder.withMetaData(1, OTH_1)},
+                {OtherTypesHolder.withMetaData(1, OTH_2)},
+                //{OtherTypesHolder.withMetaData(1, binary(ignite, OTH_1))},
+                {OtherTypesHolder.withMetaData(1, binary(ignite, OTH_2))}
         };
     }
 
     public static Object[][] provideKVMetaList(Ignite ignite) {
         return new Object[][] {
-                data(PrimitivesHolder.withMetaData(1, PH_1),
-                        PrimitivesHolder.withMetaData(2, PH_2)),
-                data(PrimitivesHolder.withMetaData(1, binary(ignite, PH_1)),
-                        PrimitivesHolder.withMetaData(2, binary(ignite, PH_2))),
-                data(PrimitiveWrappersHolder.withMetaData(1, PWH_1),
+                {asList(PrimitivesHolder.withMetaData(1, PH_1),
+                        PrimitivesHolder.withMetaData(2, PH_2))},
+                {asList(PrimitivesHolder.withMetaData(1, binary(ignite, PH_1)),
+                        PrimitivesHolder.withMetaData(2, binary(ignite, PH_2)))},
+                {asList(PrimitiveWrappersHolder.withMetaData(1, PWH_1),
                         PrimitiveWrappersHolder.withMetaData(2, PWH_2),
-                        PrimitiveWrappersHolder.withMetaData(3, PWH_3)),
-                data(PrimitiveWrappersHolder.withMetaData(1, binary(ignite, PWH_1)),
+                        PrimitiveWrappersHolder.withMetaData(3, PWH_3))},
+                {asList(PrimitiveWrappersHolder.withMetaData(1, binary(ignite, PWH_1)),
                         PrimitiveWrappersHolder.withMetaData(2, binary(ignite, PWH_2)),
-                        PrimitiveWrappersHolder.withMetaData(3, binary(ignite, PWH_3))),
-                data(OtherTypesHolder.withMetaData(1, OTH_1),
-                        OtherTypesHolder.withMetaData(2, OTH_2)),
-                data(//OtherTypesHolder.withMetaData(1, binary(ignite, OTH_1)),
-                        OtherTypesHolder.withMetaData(2, binary(ignite, OTH_2)))
+                        PrimitiveWrappersHolder.withMetaData(3, binary(ignite, PWH_3)))},
+                {asList(OtherTypesHolder.withMetaData(1, OTH_1),
+                        OtherTypesHolder.withMetaData(2, OTH_2))},
+                {asList(//OtherTypesHolder.withMetaData(1, binary(ignite, OTH_1)),
+                        OtherTypesHolder.withMetaData(2, binary(ignite, OTH_2)))}
         };
     }
 
     private DataProviders() {
-    }
-
-    private static Object[] data(Object... objects) {
-        return new Object[]{Arrays.asList(objects)};
     }
 
     private static <T> T binary(Ignite ignite, T obj) {
