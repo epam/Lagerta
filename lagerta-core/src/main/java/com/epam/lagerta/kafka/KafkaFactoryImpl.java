9f7bbaf3033a808ad4b8b7ac96bdac8321872c32
Andrei_Yakushin <Andrei_Yakushin@epam.com>
2017-04-24 16:44:39 +0400

    137 removed closeable

--- lagerta-core/src/main/java/com/epam/lagerta/kafka/KafkaFactoryImpl.java
+++ lagerta-core/src/main/java/com/epam/lagerta/kafka/KafkaFactoryImpl.java
@@ -20,36 +20,16 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 
-import java.io.Closeable;
-import java.io.IOException;
 import java.util.Properties;
-import java.util.Queue;
-import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class KafkaFactoryImpl implements KafkaFactory {
-    private final Queue<Closeable> closeables = new ConcurrentLinkedQueue<>();
-
-    public void closeAll() {
-        while (!closeables.isEmpty())
-            try {
-                closeables.poll().close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-    }
-
     @Override
     public <K, V> Producer<K, V> producer(Properties properties) {
-        return add(new KafkaProducer<>(properties));
+        return new KafkaProducer<>(properties);
     }
 
     @Override
     public <K, V> Consumer<K, V> consumer(Properties properties) {
         return new KafkaConsumer<>(properties);
-    }
-
-    private <T extends Closeable> T add(T t) {
-        closeables.add(t);
-        return t;
     }
 }
