04d3150074231ed05aa1050ba4cd6681d6e1908c
Anton Solovev <Anton_Solovev@epam.com>
2017-04-28 17:13:08 +0400

    148 [DEV] configured ssh and root password

--- lagerta-jepsen/docker/node/init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir -p /var/run/sshd
-sed -i "s/UsePrivilegeSeparation.*/UsePrivilegeSeparation no/g" /etc/ssh/sshd_config
-sed -i 's/PermitRootLogin without-password/PermitRootLogin yes/' /etc/ssh/sshd_config
-touch /root/.Xauthority true
-
-chmod 0755 /var/run/sshd
-/usr/sbin/sshd -D
