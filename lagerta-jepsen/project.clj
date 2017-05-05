a256986375aff68b94821a3a34e9defd07084a79
Roman Maier <roman_maier@epam.com>
2017-05-05 18:05:50 +0400

    removed unnecessary code

--- lagerta-jepsen/project.clj
+++ lagerta-jepsen/project.clj
@@ -20,6 +20,5 @@
   :license {:name "The Apache Software License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.txt"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [jepsen "0.1.4"]
-                 [verschlimmbesserung "0.1.3"]]
+                 [jepsen "0.1.4"]]
   :main jepsen.lagerta.core)
